ade4ff1ff243b8e4947875214ee201102790a9d9
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2023-03-25 11:08:19 +0530

    Create index.js

---
+++ CommonJSvsES6/index.js
@@ -0,0 +1,10 @@
+//This is CommonJS Practices to require the module
+ const requireCommonJS = require('./CommonJSModule')
+ console.log('This is CommonJS Practice to require the module');
+console.log(requireCommonJS); 
+
+
+//This is ES6 Practices to import the module
+import ES6Module from './ES6Module';
+console.log('This is ES6 Practice to require the module');
+console.log(ES6Module);
