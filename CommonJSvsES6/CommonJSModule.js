28172d5a0da244cef5960d233b5063fe310da389
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2023-03-25 11:05:52 +0530

    Create CommonJSModule.js

---
+++ CommonJSvsES6/CommonJSModule.js
@@ -0,0 +1,11 @@
+//This is CommonJS Practices to require the module
+// and it will return like object
+
+const fName = "Abhi"
+const lName = "Joshi"
+
+const FullName = fName + lName;
+
+module.exports = {
+    FullName
+}
