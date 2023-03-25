0d538831315517de8d7973299caddf8bc837053e
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2023-03-25 10:23:10 +0530

    Create index.js

---
+++ ImportExportFunction/index.js
@@ -0,0 +1,10 @@
+const ArithmaticOperations = require("./ArithmeticOperations");
+console.log(ArithmaticOperations);
+
+// Calling Functions of ArithmaticOperations
+
+console.log(`Addition is `, ArithmaticOperations.Addition(20, 5));
+console.log('Subtraction is ',ArithmaticOperations.Substraction(20, 5));
+console.log('Multiplication is',ArithmaticOperations.Multiplication(20, 5));
+console.log('Division is', ArithmaticOperations.Division(20, 5));
+
